eca1bda4dc905f27ae32980d0f59f1d288ab3040
IqrarH <iqrarhussain471@gmail.com>
2022-10-03 13:18:57 +0500

    signup API

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -7,6 +8,23 @@ def home(request):
     return render(request, "authentication/index.html")
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+
+
+
     return render(request, "authentication/signup.html")
 
 def signin(request):
