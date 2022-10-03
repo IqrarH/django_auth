f4345ac7511764a4d2834aa732fc17e0b04b9928
IqrarH <iqrarhussain471@gmail.com>
2022-10-03 14:15:27 +0500

    Login API

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 def home(request):
@@ -33,6 +35,17 @@ def signin(request):
     if request.method == "POST":
         username = request.POST['username']
         pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, 'authentication/index.html', {'fname': fname})
+        else:
+            messages.error(request, "Incorrect username OR password")
+            return redirect('home')
+
 
     return render(request, "authentication/signin.html")
 
